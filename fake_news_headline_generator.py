0eeb0e4f2b7852128e543f7f37af61bd5fe15545
kailashchaudhary-p <chaudharykailash185@gmail.com>
2025-07-29 12:16:08 -0800

    update

--- fake_news_headline_generator.py
+++ fake_news_headline_generator.py
@@ -1,26 +1,26 @@
 import random
 subject = [
-    "Vicky Kaushal"
-    "Karan Aujla"
-    "Virat Koli"
-    "Cat"
-    "Dog"
+    "Vicky Kaushal",
+    "Karan Aujla",
+    "Virat Koli",
+    "Cat",
+    "Dog",
     "Scorpio"
 ]
 action = [
-    "jump From"
-    "Singing a Song on"
-    "Play Gulli Danda"
-    "Jump From"
-    "See Movie"
+    "jump From",
+    "Singing a Song on",
+    "Play Gulli Danda",
+    "Jump From",
+    "See Movie",
     "Read Book"
 ] 
 place = [
-    "Kullu"
-    "bad"
-    "Bathroom"
-    "Mumbai"
-    "Goa"
+    "Kullu",
+    "bad",
+    "Bathroom",
+    "Mumbai",
+    "Goa",
     "Jamuna"
 ]
 while True:
@@ -28,6 +28,11 @@ while True:
     action=random.choice(action)
     place=random.choice(place)
 
-    Heading=
+    Heading=f"BREAKING NEWS : {subject},{action},{place}"
+    print("\n"+Heading)
+    user_input=("\n Do You Want Another Headline yes/No".strip().lower())
+    if user_input=="no":
+        break
+print("\n Thanks For Using Fake Headline Genrator Have a Fun Day")
 
 
