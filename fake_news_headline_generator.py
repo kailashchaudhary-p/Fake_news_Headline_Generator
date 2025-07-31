5818bddd5d91d8352cd7bb8268a7323a82bd5c63
kailashchaudhary-p <chaudharykailash185@gmail.com>
2025-07-30 21:58:07 -0800

    update

--- fake_news_headline_generator.py
+++ fake_news_headline_generator.py
@@ -1,4 +1,6 @@
 import random
+
+# List of subjects (people or things involved in the headline)
 subject = [
     "Vicky Kaushal",
     "Karan Aujla",
@@ -7,6 +9,8 @@ subject = [
     "Dog",
     "Scorpio"
 ]
+
+# List of actions to be randomly chosen
 action = [
     "jump From",
     "Singing a Song on",
@@ -15,6 +19,8 @@ action = [
     "See Movie",
     "Read Book"
 ] 
+
+# List of places for the action to occur
 place = [
     "Kullu",
     "bad",
@@ -23,16 +29,26 @@ place = [
     "Goa",
     "Jamuna"
 ]
+
+# Infinite loop to keep generating headlines until user says "no"
 while True:
-    subject=random.choice(subject)
-    action=random.choice(action)
-    place=random.choice(place)
+    # Randomly choose one item from each list
+    random_subject = random.choice(subject)
+    random_action = random.choice(action)
+    random_place = random.choice(place)
 
-    Heading=f"BREAKING NEWS : {subject},{action},{place}"
-    print("\n"+Heading)
-    user_input=("\n Do You Want Another Headline yes/No".strip().lower())
-    if user_input=="no":
+    # Create the fake breaking news headline
+    heading = f"BREAKING NEWS: {random_subject} {random_action} {random_place}!"
+    
+    # Print the headline
+    print("\n" + heading)
+
+    # Ask the user if they want another headline
+    user_input = input("\nDo You Want Another Headline? Yes/No: ").strip().lower()
+    
+    # If the user says 'no', exit the loop
+    if user_input == "no":
         break
-print("\n Thanks For Using Fake Headline Genrator Have a Fun Day")
 
-
+# Final thank-you message
+print("\nThanks For Using Fake Headline Generator. Have a Fun Day!")
